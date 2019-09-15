cc3f24425f3a90b2f4fb3ef995f113c7f45d823c
Jon Thornton <thornton.jon@gmail.com>
2019-09-14 23:38:12 -0400

    Update protobuf and dependencies

--- mtaproto/feedresponse.py
+++ mtaproto/feedresponse.py
@@ -7,7 +7,7 @@ TZ = timezone('US/Eastern')
 class FeedResponse(object):
 
     def __init__(self, response_string):
-        self._pb_data = nyct_subway_pb2.gtfs_realtime_pb2.FeedMessage()
+        self._pb_data = nyct_subway_pb2.gtfs__realtime__pb2.FeedMessage()
         self._pb_data.ParseFromString(response_string)
 
     def __getattr__(self, name):
