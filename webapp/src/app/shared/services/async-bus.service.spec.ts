177924c321c3003729064a8c5e5d77a858a2a738
Samuel Oh <samuel@SamuelscBookPro.lan>
2021-11-13 11:17:32 -0500

    Bus tab functional (temp. fix)

--- webapp/src/app/shared/services/async-bus.service.spec.ts
+++ webapp/src/app/shared/services/async-bus.service.spec.ts
@@ -1,16 +1,71 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {catchError, map} from "rxjs/operators";
 
-import { AsyncBusService } from './async-bus.service';
+//let BusRestAPIUrl = 'http://' + self.location.host.split(':')[0] +':5000'
+let BusRestAPIUrl = 'http://127.0.0.1:5000/bus';
 
-describe('AsyncBusService', () => {
-  let service: AsyncBusService;
+@Injectable({
+  providedIn: 'root'
+})
+export class AsyncBusService {
+  constructor(private http: HttpClient) { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AsyncBusService);
-  });
+  byLocation(latitude: number, longitude: number, num: number): Observable<any> {
+    let apiUrl = BusRestAPIUrl + "/by-location";
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    params.set("lat", latitude);
+    params.set("lon", longitude);
+    params.set("num", num);
+
+    return this.executeRequest(apiUrl, headers, params);
+  }
+
+  byRoute(route: string): Observable<any> {
+    let apiUrl = BusRestAPIUrl + "/by-route/" + route;
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+
+    return this.executeRequest(apiUrl, headers, params);
+  }
+
+  byId(ids: any[]): Observable<any> {
+    let apiUrl = BusRestAPIUrl + "/by-id/"
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+
+    // construct the correct url for by-id
+    for (let i = 0; i < ids.length; i++) {
+      if (i == ids.length - 1) {
+        apiUrl += ids[i];
+      } else {
+        apiUrl += ids[i] + ",";
+      }
+    }
+
+    return this.executeRequest(apiUrl, headers, params);
+  }
+
+  routes(): Observable<any> {
+    let apiUrl = BusRestAPIUrl + "/routes";
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+
+    return this.executeRequest(apiUrl, headers, params);
+  }
+
+  executeRequest(apiUrl: string, headers: any, params: any): Observable<any> {
+    return this.http.post<any>(apiUrl, {}, ({params: params}))
+        .pipe(map((response: Response) => { return response; }),
+            catchError((error: any) => this.handleError(error)))
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.log("request error")
+    console.log(error)
+    return Promise.reject(error.error || error)
+  }
+}
