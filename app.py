d85e63545a0397ef30d4434abd617aab2e928ce4
Jon Thornton <thornton.jon@gmail.com>
2023-08-08 22:42:31 -0400

    Upgrade flask to 2.3.2

--- app.py
+++ app.py
@@ -10,8 +10,8 @@
 """
 
 from mtapi.mtapi import Mtapi
-from flask import Flask, request, jsonify, render_template, abort, redirect
-from flask.json import JSONEncoder
+from flask import Flask, request, Response, render_template, abort, redirect
+import json
 from datetime import datetime
 from functools import wraps, reduce
 import logging
@@ -39,8 +39,7 @@ if app.debug:
     logging.basicConfig(level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-class CustomJSONEncoder(JSONEncoder):
-
+class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         try:
             if isinstance(obj, datetime):
@@ -51,7 +50,6 @@ class CustomJSONEncoder(JSONEncoder):
         else:
             return list(iterable)
         return JSONEncoder.default(self, obj)
-app.json_encoder = CustomJSONEncoder
 
 mta = Mtapi(
     app.config['MTA_KEY'],
@@ -61,10 +59,18 @@ mta = Mtapi(
     expires_seconds=app.config['CACHE_SECONDS'],
     threaded=app.config['THREADED'])
 
-def cross_origin(f):
+def response_wrapper(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         resp = f(*args, **kwargs)
+
+        if not isinstance(resp, Response):
+            # custom JSON encoder; this is important
+            resp = Response(
+                response=json.dumps(resp, cls=CustomJSONEncoder),
+                status=200,
+                mimetype="application/json"
+            )
 
         if app.config['DEBUG']:
             resp.headers['Access-Control-Allow-Origin'] = '*'
@@ -76,31 +82,31 @@ def cross_origin(f):
     return decorated_function
 
 @app.route('/')
-@cross_origin
+@response_wrapper
 def index():
-    return jsonify({
+    return {
         'title': 'MTAPI',
         'readme': 'Visit https://github.com/jonthornton/MTAPI for more info'
-        })
+        }
 
 @app.route('/by-location', methods=['GET'])
-@cross_origin
+@response_wrapper
 def by_location():
     try:
         location = (float(request.args['lat']), float(request.args['lon']))
     except KeyError as e:
         print(e)
-        response = jsonify({
-            'error': 'Missing lat/lon parameter'
-            })
-        response.status_code = 400
-        return response
+        return Response(
+            response=json.dumps({'error': 'Missing lat/lon parameter'}),
+            status=400,
+            mimetype="application/json"
+        )
 
     data = mta.get_by_point(location, 5)
     return _make_envelope(data)
 
 @app.route('/by-route/<route>', methods=['GET'])
-@cross_origin
+@response_wrapper
 def by_route(route):
 
     if route.islower():
@@ -113,7 +119,7 @@ def by_route(route):
         abort(404)
 
 @app.route('/by-id/<id_string>', methods=['GET'])
-@cross_origin
+@response_wrapper
 def by_index(id_string):
     ids = id_string.split(',')
     try:
@@ -123,12 +129,12 @@ def by_index(id_string):
         abort(404)
 
 @app.route('/routes', methods=['GET'])
-@cross_origin
+@response_wrapper
 def routes():
-    return jsonify({
+    return {
         'data': sorted(mta.get_routes()),
         'updated': mta.last_update()
-        })
+        }
 
 def _envelope_reduce(a, b):
     if a['last_update'] and b['last_update']:
@@ -143,10 +149,10 @@ def _make_envelope(data):
     if data:
         time = reduce(_envelope_reduce, data)['last_update']
 
-    return jsonify({
+    return {
         'data': data,
         'updated': time
-        })
+    }
 
 if __name__ == '__main__':
     app.run(use_reloader=False)
